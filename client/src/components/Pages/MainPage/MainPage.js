920c4a8d1ae43a6138ac209755a1493926a7d2e7
heleneyj <heleneyj@stud.ntnu.no>
2020-01-07 15:50:19 +0100

    fix boostrap

--- client/src/components/Pages/MainPage/MainPage.js
+++ client/src/components/Pages/MainPage/MainPage.js
@@ -5,7 +5,11 @@ class MainPage extends Component{
     render() {
         return (
             <div>
-                <h1> hello </h1>
+                <div id="MainPageDiv">
+                    <h1> Harmoni </h1>
+                    <button type="button" className="btn btn-primary">log in</button>
+                    <button type="button" className="btn btn-secondary">register</button>
+                </div>
             </div>
         );
     }

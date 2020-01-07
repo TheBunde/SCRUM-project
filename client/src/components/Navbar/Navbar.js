d4bfb6b8bd88cc6a7bf270ffa2ee4a310acfd4f6
Martin Johannes Nilsen <martijni@stud.ntnu.no>
2020-01-07 17:22:36 +0100

    Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import '../../css/Navbar.css';
+import $ from 'jquery'; 
 
 
 class Navbar extends Component{
@@ -8,29 +9,26 @@ class Navbar extends Component{
         return (
             <div>
                 <nav class="navbar navbar-expand-lg navbar-dark" id="navbar">
-                    <a class="navbar-brand" href="#"><h1>Harmoni</h1></a>
+                    <a class="navbar-brand" href="#/overview"><h1>Harmoni</h1></a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <a class="nav-link" onClick={()=> window.location.href="#/Entreprenør"} data-toggle="collapse" data-target="#navbarSupportedContent">Ivar Nilsen Entreprenør AS<span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" onClick={()=> window.location.href="#/Eiendom"} data-toggle="collapse" data-target="#navbarSupportedContent"href="#/Eiendom">Ivar Nilsen Eiendom AS</a>
+                            <a class="nav-link" onClick={()=> window.location.href="#/overview"} data-toggle="collapse" data-target="#navbarSupportedContent">Oversikt</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#/Prosjekt" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Prosjekter
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Arrangementer
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown" id="navbarDropdown">
-                                <a class="dropdown-item" onClick={()=> window.location.href="#/prosjekt/1"} data-toggle="collapse" data-target="#navbarSupportedContent">Nordby utvikling</a>
-                                <a class="dropdown-item" onClick={()=> window.location.href="#/prosjekt/2"} data-toggle="collapse" data-target="#navbarSupportedContent">Såstad</a>
+                                <a class="dropdown-item" onClick={()=> window.location.href="#/event"} data-toggle="collapse" data-target="#navbarSupportedContent">Alle arrangementer</a>
+                                <a class="dropdown-item" onClick={()=> window.location.href="#/overview/addEvent"} data-toggle="collapse" data-target="#navbarSupportedContent">Legg til arrangement</a>
                             </div>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" onClick={()=> window.location.href="#/Kontakt"} data-toggle="collapse" data-target="#navbarSupportedContent">Kontakt oss</a>
+                            <a class="nav-link" onClick={()=> window.location.href="#/"} data-toggle="collapse" data-target="#navbarSupportedContent">Logg ut</a>
                         </li>
                         </ul>
                     </div>

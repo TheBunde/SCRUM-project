faf505c3e8602232ddafe4ce4ca4429d75320af9
Martin Johannes Nilsen <martijni@stud.ntnu.no>
2020-01-08 12:52:34 +0100

    Added an about-page with an suggestion for mainpage

--- client/src/index.js
+++ client/src/index.js
@@ -18,6 +18,7 @@ import EventPage from './components/Pages/EventPage/EventPage.js';
 import EventView from './components/Pages/EventView/EventView.js';
 import EditEvent from './components/Pages/EditEvent/EditEvent.js';
 import AddEvent from './components/Pages/AddEvent/AddEvent.js';
+import About from './components/About/About.js';
 
 ReactDOM.render(
     <HashRouter>
@@ -30,6 +31,7 @@ ReactDOM.render(
             <Route exact path="/event/:id" component={EventView} />
             <Route exact path="/event/:id/edit" component={EditEvent} />
             <Route exact path="/overview/addEvent" component={AddEvent} />
+            <Route exact path="/about" component={About} />
             <ToTop />
             <Footer />
         </div>

7275ebfd2884c27ad18f61cc1c5b9e2c54f537e2
Martin Johannes Nilsen <martijni@stud.ntnu.no>
2020-01-08 14:09:40 +0100

    about

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -10,16 +10,15 @@ class About extends Component {
     return (
         <div>
             <NavbarMainPage/>
-
             <div id="AboutDiv">
                 <div id="AboutTitle"><img src={Logo} width="300" /></div>
-                <div id="AboutText"><h4>En portal for god informasjonsflyt under arrangementplanlegging</h4></div>
+                <div id="AboutText"><h4>En portal for informasjonsflyt under planlegging av arrangementer</h4></div>
                 <div id="AboutButtonDiv">
                     <div id="AboutButtons">
-                        <button type="button" className="btn btn-outline-light btn-lg">Logg inn</button>
+                        <button type="button" className="btn btn-outline-light btn-lg" onClick={()=> window.location.href = "#/login"}>Logg inn</button>
                     </div>
                     <div id="AboutButtons">
-                        <button type="button" className="btn btn-outline-light btn-lg">Registrer ny bruker</button>
+                        <button type="button" className="btn btn-outline-light btn-lg" onClick={()=> window.location.href = "#/register"}>Registrer ny bruker</button>
                     </div> 
                 </div>  
             </div>

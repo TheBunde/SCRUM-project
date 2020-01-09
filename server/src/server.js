9f4abb352566eb0d5b64937f3dff722f120b8a2a
tblauvvik <tb@lauvvik.no>
2020-01-09 09:06:20 +0100

    Testing login
    -added .env file

--- server/src/server.js
+++ server/src/server.js
@@ -3,16 +3,18 @@ let mysql = require("mysql");
 let app = express();
 let bodyParser = require("body-parser");
 const UserDao = require("./dao/UserDao");
+const dotenv = require('dotenv');
+dotenv.config();
 
 
 app.use(bodyParser.json()); // for å tolke JSON
 
 let pool = mysql.createPool({ 
     connectionLimit: 5,
-    host: "mysql.stud.iie.ntnu.no", 
-    user: "g_scrum_5",
-    password: "TYQHbYDq", 
-    database: "g_scrum_5", 
+    host: process.env.DB_URL,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
     debug: false
 });
 
@@ -32,5 +34,40 @@ app.post("/users", (req, res) => {
         res.json(data);
     });
 });
+
+app.get("/validate", (req, res) => {
+    console.log("/login request");
+    let userSalt = userDao.getSalt(req.body.email, (status, data) => {
+        res.status(status);
+        res.json(data);
+        //console.log(res);
+    });
+
+    let salt = "123";
+
+    let dbHash = userDao.getHash(req.body.email, (status, data) => {
+        res.status(status);
+        res.json(data);
+    });
+
+    let hash = userDao.hashPassword(req.body.password, salt);
+
+
+    console.log("Hash : "+hash.passwordHash);
+    console.log(userSalt);
+
+
+    if (dbHash === hash) {
+        console.log("Password is OK");
+        //res.json({ "passwordOK": true });
+        //res.status(200);
+    } else {
+        console.log("Password not ok");
+        //res.json({ "passwordOK": false });
+    }
+});
+
+
+
 
 let server = app.listen(8080);

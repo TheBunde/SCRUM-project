82859ea2127205eb68bd59704ae52393d13084b1
Martin Johannes Nilsen <martijni@stud.ntnu.no>
2020-01-09 13:01:24 +0100

    Fjernet .idea

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -6,24 +6,28 @@ import Logo from '../../img/Logo1.png'
 import {NavbarMainPage} from "../Navbar/Navbar";
 
 class About extends Component {
-  render() {  
-    return (
-        <div>
-            <div id="AboutDiv">
-                <div id="AboutTitle"><a>HARMONI</a></div>
-                <div id="AboutText"><h4>En portal for informasjonsflyt under planlegging av arrangementer</h4></div>
-                <div id="AboutButtonDiv">
-                    <div id="AboutButtons">
-                        <button type="button" className="btn btn-outline-light btn-lg" onClick={()=> window.location.href = "#/login"}>Logg inn</button>
-                    </div>
-                    <div id="AboutButtons">
-                        <button type="button" className="btn btn-outline-light btn-lg" onClick={()=> window.location.href = "#/register"}>Registrer ny bruker</button>
-                    </div> 
-                </div>          
+    componentDidMount(){
+        window.scrollTo(0,0);
+    }
+
+    render() {  
+        return (
+            <div>
+                <div id="AboutDiv">
+                    <div id="AboutTitle"><a>HARMONI</a></div>
+                    <div id="AboutText"><h4>En portal for informasjonsflyt under planlegging av arrangementer</h4></div>
+                    <div id="AboutButtonDiv">
+                        <div id="AboutButtons">
+                            <button type="button" className="btn btn-outline-light btn-lg" onClick={()=> window.location.href = "#/login"}>Logg inn</button>
+                        </div>
+                        <div id="AboutButtons">
+                            <button type="button" className="btn btn-outline-light btn-lg" onClick={()=> window.location.href = "#/register"}>Registrer ny bruker</button>
+                        </div> 
+                    </div>          
+                </div>
             </div>
-        </div>
-    )
-  }
+        )
+    }
 }
 
 export default About;

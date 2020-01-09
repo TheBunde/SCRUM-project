2f4e1bb8cedb95ff3e7d880064a27cc108bd1c4b
Simon Årdal <simoaa@stud.ntnu.no>
2020-01-09 10:26:58 +0100

    fix adminservice

--- client/src/service/AdminService.js
+++ client/src/service/AdminService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-class AdminServices{
+class AdminService{
 
     getUsers(){
         return axios.get("http://localhost:8080/users/").then(response => response.data);

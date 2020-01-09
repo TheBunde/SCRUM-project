0f89806f1fa0067a9d97b7528e6e4d449ce76417
Mathias <mathiaao@stud.ntnu.no>
2020-01-09 13:54:51 +0100

    Changes to AddEvent

--- client/src/service/EventService.js
+++ client/src/service/EventService.js
@@ -4,7 +4,7 @@ import event from '../components/Pages/EventPage/EventPage.js';
 class EventService{
 
     addEvents(event_id, name, date, description, place, artists, tech_rider, hospitality_rider, personnel, category_id, filed, pending, img_url){
-        let newEvent = new event(event_id, name, date, description, place, artists, tech_rider, hospitality_rider, personnel, category_id, filed, pending, img_url);
+        let newEvent = new Event(name, date, description, place, artists, tech_rider, hospitality_rider, personnel, img_url);
         return axios.post("http://localhost:8080/event/", newEvent).then(response => response.data);
     }
 
@@ -13,7 +13,7 @@ class EventService{
     }
 
     getEventById(eventID){
-        return axios.get("http://localhost:8080/event/" + event_id).then(response => response.data);
+        return axios.get("http://localhost:8080/event/" + eventID).then(response => response.data);
     }
     
 }

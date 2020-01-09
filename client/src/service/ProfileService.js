1ce92c1f7064bbd5b797b790e5e3039e463ae061
heleneyj <heleneyj@stud.ntnu.no>
2020-01-09 15:58:38 +0100

    fix

--- client/src/service/ProfileService.js
+++ client/src/service/ProfileService.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 class ProfileService{
 
     updateUser(user){
+        console.log("It is fixed");
         return axios.put('http://localhost:8080/profile/' + user.user_id + '/edit', user);
     }
 

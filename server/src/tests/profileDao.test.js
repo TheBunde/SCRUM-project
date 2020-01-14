1dead00b9cd0ea448ed0118ab6106a1da4503b6a
heleneyj <heleneyj@stud.ntnu.no>
2020-01-14 10:00:28 +0100

    profileDaoTest fix

--- server/src/tests/profileDao.test.js
+++ server/src/tests/profileDao.test.js
@@ -19,13 +19,21 @@ let userDao = new UserDao(pool);
 
 
 test('Changing contact information', done => {
-    function callback(status, data) {
+    function callback2(status, data) {
         console.log('Test callback: status=' + status + ', data=' + JSON.stringify(data));
         expect(data.affectedRows).toBeGreaterThanOrEqual(1);
+        expect(data[0].user_id).toBe(3);
+        expect(data[0].name).toBe('Grete');
+        expect(data[0].phone).toBe('09876543');
+        expect(data[0].email).toBe('new@mail.com');
         done();
     }
 
-    userDao.updateProfile({ name: 'Grete', tlf: '09876543', email : 'new@mail.com', user_id: 3 }, callback);
+    function callback(status, data){
+        adminDao.getUser(3, callback2);
+    }
+
+    userDao.updateProfile({ name: 'Grete', phone: '09876543', email : 'new@mail.com', user_id: 3 }, callback);
 });
 
 

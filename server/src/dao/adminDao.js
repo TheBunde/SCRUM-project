bf984e6b8bb318eea085d7d84bf367cb6fadcd25
heleneyj <heleneyj@stud.ntnu.no>
2020-01-15 10:27:00 +0100

    New adminTest

--- server/src/dao/adminDao.js
+++ server/src/dao/adminDao.js
@@ -2,18 +2,6 @@ const Dao = require("./dao.js");
 
 module.exports = class adminDao extends Dao{
 
-    getRole(role, callback){
-        super.query("SELECT role_id FROM Role WHERE role=?", [role], callback);
-    }
-
-    getRoleById(roleID, callback){
-        super.query("SELECT role FROM Role WHERE role_id=?", [roleID], callback);
-    }
-
-    getRoles(callback){
-        super.query("SELECT role FROM Role", [], callback);
-    }
-
     getUser(userID, callback){
         super.query("SELECT * FROM User WHERE user_id=?", [userID], callback);
     }
@@ -26,15 +14,28 @@ module.exports = class adminDao extends Dao{
         super.query("UPDATE User SET role_id=? WHERE user_id=?", [parseInt(roleId), parseInt(userID)], callback);
     }
 
-    deleteUser(userID, callback){
-        super.query("DELETE FROM User WHERE user_id=?", [userID], callback);
-    }
-
     approveUser(userID, callback){
         super.query("UPDATE User SET approved=1 WHERE user_id=?", [userID], callback);
+    }
+
+    deleteUser(userID, callback){
+        super.query("DELETE FROM User WHERE user_id=?", [userID], callback);
     }
 
     disapproveUser(userID, callback){
         super.query("UPDATE User SET approved=0 WHERE user_id=?", [userID], callback);
     }
+
+    getRole(role, callback){
+        super.query("SELECT role_id FROM Role WHERE role=?", [role], callback);
+    }
+
+    getRoleById(roleID, callback){
+        super.query("SELECT role FROM Role WHERE role_id=?", [roleID], callback);
+    }
+
+    getRoles(callback){
+        super.query("SELECT role FROM Role", [], callback);
+    }
+
 };

bcfe6e66c1aeaa206b87957d4c2a7c1f3441fb41
Simon Årdal <simoaa@stud.ntnu.no>
2020-01-17 13:46:05 +0100

    designed front main page

--- client/src/components/Pages/Guest/GuestMainPage/GuestMainPage.js
+++ client/src/components/Pages/Guest/GuestMainPage/GuestMainPage.js
@@ -15,10 +15,11 @@ class GuestMainPage extends Component {
         return (
             <div>
                 <div id="GuestMainPageContainer">
+                    <h1>Arrangementer</h1>
                     <div className={"cardsContainer"}>
                         {this.state.events.map(event => (
                             <GuestEventCard key={event.event_id} name = {event.name} description = {event.description} date={event.date}
-                            place = {event.place} img_url = {event.img_url} artists = {event.artists}/>))
+                            place = {event.place} img_url = {event.img_url}/>))
                         }
                     </div>
                     <button type="button" className="btn btn-outline-dark btn-lg" onClick={() => window.location.href="#/portal"}>Arrangørportal</button>

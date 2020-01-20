e56d103b08082af3b4b33b4d7e7172215fa82712
lisawil <lisawil@stud.ntnu.no>
2020-01-20 10:12:50 +0100

    nordlys bilde

--- server/src/server.js
+++ server/src/server.js
@@ -37,7 +37,6 @@ let pool = mysql.createPool({
 
 
 
-
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from

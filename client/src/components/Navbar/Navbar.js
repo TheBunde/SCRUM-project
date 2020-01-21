62307ee4caf34f97410d791613e6ca051087a94e
Torbjørn Bøe Lauvvik <34974950+tblauvvik@users.noreply.github.com>
2020-01-21 22:53:57 +0100

    fix

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,126 +1,184 @@
-import React, {Component} from 'react';
-import '../../css/Navbar.css';
-import $ from 'jquery';
-import {auth, authenticate} from "../../service/UserService";
-import {ProfileService} from "../../service/ProfileService";
-
+import React, { Component } from "react";
+import "../../css/Navbar.css";
+import $ from "jquery";
+import { auth, authenticate } from "../../service/UserService";
+import { ProfileService } from "../../service/ProfileService";
 
 /*Changing ml-auto to mr-auto will change the placement of the navbar-collapse items to the left besides the logo/name to the left on the Navbar */
 
-
+/**
+ * Navbar component
+ */
 class Navbar extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            user : {},
-            user_id: -1
-        }
-    }
-
-    someFn(){
-        this.props.getUser(this.state.user);
-    }
-
-    signOut = () => {
-        window.localStorage.removeItem("token");
-        window.location.hash = "/login";
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      user_id: -1
     };
+  }
 
-    componentDidMount() {
-        let show = document.getElementById("adminUsersLink");
-        let style = window.getComputedStyle(show);
-        authenticate();
-        if (auth.role == "admin") {
-            show.style.display = "block";
-        } else {
-            show.style.display = "none";
-        }
-        let profileService = new ProfileService();
-        profileService.getUser(auth.user_id)
-            .then(user => {
-                    this.setState({
-                        user: user
-                    })
-                }
-            )
-            .catch((error) => {
-                console.error(error);
-            });
+  someFn() {
+    this.props.getUser(this.state.user);
+  }
+
+  signOut = () => {
+    window.localStorage.removeItem("token");
+    window.location.hash = "/login";
+  };
+
+  componentDidMount() {
+    let show = document.getElementById("adminUsersLink");
+    let style = window.getComputedStyle(show);
+    authenticate();
+    if (auth.role == "admin") {
+      show.style.display = "block";
+    } else {
+      show.style.display = "none";
     }
+    let profileService = new ProfileService();
+    profileService
+      .getUser(auth.user_id)
+      .then(user => {
+        this.setState({
+          user: user
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
-    render() {
-        return (
-            <div>
-                <nav class="navbar navbar-expand-lg navbar-dark" id="navbar">
-                    <a class="navbar-brand" href="#/"><h1>Harmoni</h1></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ml-auto">
+  render() {
+    return (
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-dark" id="navbar">
+          <a class="navbar-brand" href="#/">
+            <h1>Harmoni</h1>
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#/overview">
+                  Hovedside
+                </a>
+              </li>
 
-                            <li className="nav-item">
-                                <a className="nav-link"
-                                   href = "#/overview">Hovedside</a>
-                            </li>
+              <li className="nav-item" id={"adminUsersLink"}>
+                <a className="nav-link" href="#/admin/users">
+                  Rediger brukere
+                </a>
+              </li>
 
-                            <li className="nav-item" id={"adminUsersLink"}>
-                                <a className="nav-link" href = "#/admin/users">Rediger brukere</a>
-                            </li>
+              <li className="nav-item" id={"adminUsersLink"}>
+                <a className="nav-link" href="#/calendar">
+                  Kalender
+                </a>
+              </li>
 
-                            <li className="nav-item" id={"adminUsersLink"}>
-                                <a className="nav-link" href = "#/calendar">Kalender</a>
-                            </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Arrangementer
+                </a>
+                <div
+                  class="dropdown-menu"
+                  aria-labelledby="navbarDropdown"
+                  id="navbarDropdown"
+                >
+                  <a class="dropdown-item" href="#/event">
+                    Alle arrangementer
+                  </a>
+                  <a class="dropdown-item" href="#/overview/addEvent">
+                    Legg til arrangement
+                  </a>
+                </div>
+              </li>
 
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href = "#" role="button" data-toggle="dropdown"
-                                   aria-haspopup="true" aria-expanded="false">
-                                    Arrangementer
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown" id="navbarDropdown">
-                                    <a class="dropdown-item" href = "#/event">Alle
-                                        arrangementer</a>
-                                    <a class="dropdown-item"
-                                       href = "#/overview/addEvent">Legg til
-                                        arrangement</a>
-                                </div>
-                            </li>
-
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href = "#" role="button" data-toggle="dropdown"
-                                   aria-haspopup="true" aria-expanded="false">
-                                    <img id="navProfile" alt="profilePic" src={this.state.user.profile_photo === null || this.state.user.profile_photo === "" ? "https://www.sketchengine.eu/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" : "http://localhost:8080/image/" + this.state.user.profile_photo}
-                                         width="30" height="30"/>
-                                         {this.state.user.name}
-                                </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdown" id="navbarDropdown">
-                                    <a className="dropdown-item"
-                                       href = {"#/profile/" + auth.user_id}>Min profil</a>
-                                    <a className="dropdown-item"
-                                    href = {"#/profile/" + auth.user_id + "/edit"}>Rediger profil</a>
-                                    <div className="dropdown-divider"></div>
-                                    <a className="dropdown-item"
-                                       href = {"#/about"}>Om</a>
-                                    <a className="dropdown-item" href="mailto:noreply.harmoni@gmail.com?Subject=Hello%20again">
-                                        Kontakt oss</a>
-                                    <div className="dropdown-divider"></div>
-                                    <a className="dropdown-item"
-                                       onClick={() => this.signOut()} href="#">Logg ut</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        );
-    }
-
-
-
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <img
+                    id="navProfile"
+                    alt="profilePic"
+                    src={
+                      this.state.user.profile_photo === null ||
+                      this.state.user.profile_photo === ""
+                        ? "https://www.sketchengine.eu/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png"
+                        : "http://localhost:8080/image/" +
+                          this.state.user.profile_photo
+                    }
+                    width="30"
+                    height="30"
+                  />
+                  {this.state.user.name}
+                </a>
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdown"
+                  id="navbarDropdown"
+                >
+                  <a
+                    className="dropdown-item"
+                    href={"#/profile/" + auth.user_id}
+                  >
+                    Min profil
+                  </a>
+                  <a
+                    className="dropdown-item"
+                    href={"#/profile/" + auth.user_id + "/edit"}
+                  >
+                    Rediger profil
+                  </a>
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item" href={"#/about"}>
+                    Om
+                  </a>
+                  <a
+                    className="dropdown-item"
+                    href="mailto:noreply.harmoni@gmail.com?Subject=Hello%20again"
+                  >
+                    Kontakt oss
+                  </a>
+                  <div className="dropdown-divider"></div>
+                  <a
+                    className="dropdown-item"
+                    onClick={() => this.signOut()}
+                    href="#"
+                  >
+                    Logg ut
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
-
--- client/src/service/FileService.js
+++ client/src/service/FileService.js
@@ -1,55 +1,63 @@
 import axios from "axios";
-import {authenticationHeader} from "./UserService";
+import { authenticationHeader } from "./UserService";
 let ipAdress = process.env.REACT_APP_HOSTNAME || "localhost";
 
+/**
+ * Class to handle file services
+ */
 export class FileService {
-    uploadImage(image) {
-        const url = "http://" + ipAdress + ":8080/upload";
-        const formData = new FormData();
-        formData.append("file", image);
-        const config = {
-            headers: {
-                'Content-Type': "multipart/form-data"
-            }
-        };
-        return axios.post(url, formData, config);
-    }
+  /**
+   *
+   * @param {image} image - Takes a image
+   */
+  uploadImage(image) {
+    const url = "http://" + ipAdress + ":8080/upload";
+    const formData = new FormData();
+    formData.append("file", image);
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    };
+    return axios.post(url, formData, config);
+  }
+  /**
+   *
+   * @param {file} files - Takes multiple files
+   */
+  uploadFiles(files) {
+    const url = "http://" + ipAdress + ":8080/uploadFiles";
+    const formData = new FormData();
+    files.forEach(file => {
+      formData.append("files", file);
+    });
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    };
+    return axios.post(url, formData, config);
+  }
+  /**
+   *
+   * @param {file} file - Takes one file
+   */
+  uploadFile(file) {
+    const url = "http://" + ipAdress + ":8080/uploadFile";
+    const formData = new FormData();
+    formData.append("file", file);
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    };
+    return axios.post(url, formData, config);
+  }
 
-    uploadFiles(files) {
-        const url = "http://" + ipAdress + ":8080/uploadFiles";
-        const formData = new FormData;
-        files.forEach((file) => {
-            formData.append("files", file);
-        });
-        const config = {
-            headers : {
-                'Content-Type' : "multipart/form-data"
-            }
-        };
-        return axios.post(url, formData, config);
-
-    }
-
-    uploadFile(file) {
-        const url = "http://" + ipAdress + ":8080/uploadFile";
-        const formData = new FormData();
-        formData.append("file", file);
-        const config = {
-            headers: {
-                'Content-Type': "multipart/form-data"
-            }
-        };
-        return axios.post(url, formData, config);
-    }
-
-    getFile(fileName) {
-        const url = "http://" + ipAdress + ":8080/image/" + fileName;
-        console.log("URL:");
-        console.log(url);
-        return axios.get(url);
-    }
-
-
-
-
+  getFile(fileName) {
+    const url = "http://" + ipAdress + ":8080/image/" + fileName;
+    console.log("URL:");
+    console.log(url);
+    return axios.get(url);
+  }
 }

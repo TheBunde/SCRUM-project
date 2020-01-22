d994485d62356422f02f7928c9bb4308d29a8916
olejlia <olejlia@stud.ntnu.no>
2020-01-22 13:59:22 +0100

    Small QOL fix. Now skips login if already signed in

--- server/src/server.js
+++ server/src/server.js
@@ -499,8 +499,6 @@ app.get("/tickets/:id", verifyToken,(req, res)=>{
 
 });
 
-
-
 //UPLOAD
 
 app.post('/upload', upload.single('file'), function (req, res) {
@@ -914,7 +912,6 @@ function verifyToken(req, res, next) {
         const bearerToken = bearer[1];
         req.token = bearerToken;
         next();
-
     } else {
         res.sendStatus(403);
     }

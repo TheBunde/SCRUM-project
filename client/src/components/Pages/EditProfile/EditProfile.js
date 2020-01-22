3ddce3fdb5de5eeb28f8527a89836d32b04dfd0b
Max <maxts@stud.ntnu.no>
2020-01-22 10:26:03 +0100

    Fixed error on EditProfile

--- client/src/components/Pages/EditProfile/EditProfile.js
+++ client/src/components/Pages/EditProfile/EditProfile.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import "../../../css/EditProfile.css"
-import { createHashHistory } from 'history';
+import {createHashHistory} from 'history';
 import {ProfileService} from '../../../service/ProfileService'
 import {FileService} from "../../../service/FileService";
 import {User, UserService} from "../../../service/UserService";
@@ -17,13 +17,13 @@ import Footer from "../../Footer/Footer";
 const history = createHashHistory();
 
 
-class EditProfile extends Component{
+class EditProfile extends Component {
     user_id = this.props.match.params.userID;
     user;
 
     constructor(props) {
         super(props);
-        this.state = {user : {}, img_url : ""}
+        this.state = {user: {}, img_url: ""}
 
     }
 
@@ -37,16 +37,27 @@ class EditProfile extends Component{
 
     notifyFailure = () => toast("Noe gikk galt", {type: toast.TYPE.ERROR, position: toast.POSITION.BOTTOM_LEFT});
 
-    notifyUnvalidPhone = () => toast("Ugyldig telefonnummer", {type: toast.TYPE.ERROR, position: toast.POSITION.BOTTOM_LEFT});
+    notifyUnvalidPhone = () => toast("Ugyldig telefonnummer", {
+        type: toast.TYPE.ERROR,
+        position: toast.POSITION.BOTTOM_LEFT
+    });
 
     notifyUnvalidEmail = () => toast("Ugyldig e-post", {type: toast.TYPE.ERROR, position: toast.POSITION.BOTTOM_LEFT});
 
-    notifyMissingPassword = () => toast("Du må fylle ut begge passord-feltene", {type: toast.TYPE.ERROR, position: toast.POSITION.BOTTOM_LEFT});
+    notifyMissingPassword = () => toast("Du må fylle ut begge passord-feltene", {
+        type: toast.TYPE.ERROR,
+        position: toast.POSITION.BOTTOM_LEFT
+    });
 
-    notifyPasswordFailure = () => toast("Du har skrevet inn feil nåværende passord", {type: toast.TYPE.ERROR, position: toast.POSITION.BOTTOM_LEFT});
+    notifyPasswordFailure = () => toast("Du har skrevet inn feil nåværende passord", {
+        type: toast.TYPE.ERROR,
+        position: toast.POSITION.BOTTOM_LEFT
+    });
 
-    notifyTooBigFile = () => toast("Filen du forsøkte å laste opp var for stor", {type: toast.TYPE.ERROR, position: toast.POSITION.BOTTOM_LEFT});
-
+    notifyTooBigFile = () => toast("Filen du forsøkte å laste opp var for stor", {
+        type: toast.TYPE.ERROR,
+        position: toast.POSITION.BOTTOM_LEFT
+    });
 
 
     componentDidMount() {
@@ -78,14 +89,14 @@ class EditProfile extends Component{
 
         console.log(name === "");
 
-        if(
+        if (
             (name === null || name === "" ||
-            email === null || email === "" ||
-            phone === null || phone === "" ||
-            pw === null || pw === "" ||
-            role === null || role === "" ||
-            approved === null || approved === "")
-        ){
+                email === null || email === "" ||
+                phone === null || phone === "" ||
+                pw === null || pw === "" ||
+                role === null || role === "" ||
+                approved === null || approved === "")
+        ) {
             this.notifyFailure();
             return false;
         } else if (!(validatePhone(phone))) {
@@ -94,8 +105,7 @@ class EditProfile extends Component{
         } else if (!(validateEmail(email))) {
             this.notifyUnvalidEmail();
             return false;
-        }
-        else{
+        } else {
             return true;
         }
     };
@@ -104,64 +114,65 @@ class EditProfile extends Component{
         document.getElementById()
     }
 
-    render(){
-        return(
+    render() {
+        return (
             auth.user_id === this.props.match.params.userID ?
-            <div>
-                <Navbar/>
-                <div id="EditProfileDiv">
-                    <h1>Endre Profil</h1>
-                    <div id="ShowProfileDivide" className="dropdown-divider border-dark"></div>
+                <div>
+                    <Navbar/>
+                    <div id="EditProfileDiv">
+                        <h1>Endre Profil</h1>
+                        <div id="ShowProfileDivide" className="dropdown-divider border-dark"></div>
 
-                    <div id="EditProfileInput">
-                        <div className="col-sm-4">
-                            <h5>Navn: </h5>
-                            <input
-                                id="nameInput"
-                                className="form-control form-control-lg"
-                                type="text"
-                                defaultValue={this.state.user.name}
-                                aria-describedby="namehelp"
-                            />
-                            <br/>
+                        <div id="EditProfileInput">
+                            <div className="col-sm-4">
+                                <h5>Navn: </h5>
+                                <input
+                                    id="nameInput"
+                                    className="form-control form-control-lg"
+                                    type="text"
+                                    defaultValue={this.state.user.name}
+                                    aria-describedby="namehelp"
+                                />
+                                <br/>
+                            </div>
+
+                            <div className="col-sm-4">
+                                <h5>Telefon: </h5>
+                                <input
+                                    id="tlfInput"
+                                    className="form-control form-control-lg"
+                                    type="text"
+                                    defaultValue={this.state.user.phone}
+                                    aria-describedby="tlfhelp"
+                                />
+                                <br/>
+                            </div>
+
+
+                            <div className="col-sm-4">
+                                <h5>E-post: </h5>
+                                <input
+                                    id="emailInput"
+                                    className="form-control form-control-lg"
+                                    type="text"
+                                    defaultValue={this.state.user.email}
+                                    aria-describedby="emailhelp"
+                                />
+                                <br/>
+                            </div>
+                            <div className="col-sm-4">
+                                <h5>Profilbilde: </h5>
+                                <input
+                                    id="fileInput"
+                                    className="form-control form-control-lg"
+                                    type="file" accept={"image/*"}
+                                />
+                                <br/>
+                            </div>
                         </div>
 
-                        <div className="col-sm-4">
-                            <h5>Telefon: </h5>
-                            <input
-                                id="tlfInput"
-                                className="form-control form-control-lg"
-                                type="text"
-                                defaultValue={this.state.user.phone}
-                                aria-describedby="tlfhelp"
-                            />
-                            <br/>
-                        </div>
-
-
-                        <div className="col-sm-4">
-                            <h5>E-post: </h5>
-                            <input
-                                id="emailInput"
-                                className="form-control form-control-lg"
-                                type="text"
-                                defaultValue={this.state.user.email}
-                                aria-describedby="emailhelp"
-                            />
-                            <br/>
-                        </div>
-                        <div className="col-sm-4">
-                            <h5>Profilbilde: </h5>
-                            <input
-                                id="fileInput"
-                                className="form-control form-control-lg"
-                                type="file" accept={"image/*"}
-                            />
-                            <br/>
-                        </div>
-                    </div>
-
-                    <button type="button" className="btn btn-dark btn-lg" onClick={this.save}>Lagre endringer</button>
+                        <button type="button" className="btn btn-dark btn-lg" onClick={this.save}>Lagre endringer
+                        </button>
                         <button
                             className="btn changePassword btn-outline-info btn-lg"
                             type="button"
@@ -172,43 +183,45 @@ class EditProfile extends Component{
                         >
                             Endre passord
                         </button>
-                    <div id="EditProfileChangePasswordDiv">
-                        <div className="collapse" id="collapsechangePassword">
+                        <div id="EditProfileChangePasswordDiv">
+                            <div className="collapse" id="collapsechangePassword">
 
-                            <div className="col-sm-4">
-                                <h5>Gammelt passord: </h5>
-                                <input
-                                    id="oldPasswordInput"
-                                    className="form-control form-control-lg"
-                                    type="password"
-                                    placeholder="Gammelt passord"
-                                    aria-describedby="oldPasHelp"
-                                />
-                                <br/>
-                            </div>
+                                <div className="col-sm-4">
+                                    <h5>Gammelt passord: </h5>
+                                    <input
+                                        id="oldPasswordInput"
+                                        className="form-control form-control-lg"
+                                        type="password"
+                                        placeholder="Gammelt passord"
+                                        aria-describedby="oldPasHelp"
+                                    />
+                                    <br/>
+                                </div>
 
-                            <div className="col-sm-4">
-                                <h5>Nytt passord: </h5>
-                                <input
-                                    id="newPasswordInput"
-                                    className="form-control form-control-lg"
-                                    type="password"
-                                    placeholder="Nytt passord"
-                                    aria-describedby="newPasHelp"
-                                />
-                                <br/>
-                            </div>
-                            <div className={"col-sm-4"}>
-                                <button type="button" className="btn btn-dark btn-lg" onClick={() => this.changePW()}>Lagre nytt passord</button>
-                            </div>
+                                <div className="col-sm-4">
+                                    <h5>Nytt passord: </h5>
+                                    <input
+                                        id="newPasswordInput"
+                                        className="form-control form-control-lg"
+                                        type="password"
+                                        placeholder="Nytt passord"
+                                        aria-describedby="newPasHelp"
+                                    />
+                                    <br/>
+                                </div>
+                                <div className={"col-sm-4"}>
+                                    <button type="button" className="btn btn-dark btn-lg"
+                                            onClick={() => this.changePW()}>Lagre nytt passord
+                                    </button>
+                                </div>
 
+                            </div>
                         </div>
-                    </div>
 
+                    </div>
+                    <Footer/>
                 </div>
-                <Footer/>
-            </div>
-                : <Redirect to={"/profile/" + auth.user_id} />
+                : <Redirect to={"/profile/" + auth.user_id}/>
 
         )
     }
@@ -221,13 +234,13 @@ class EditProfile extends Component{
         const oldPWInput = document.getElementById("oldPasswordInput").value;
         const newPWInput = document.getElementById("newPasswordInput").value;
 
-        if( // If any of the fields are empty, prompt the user to fill them in before proceeding
+        if ( // If any of the fields are empty, prompt the user to fill them in before proceeding
             oldPWInput === null || oldPWInput === "",
             newPWInput === null || newPWInput === ""
-        ){
+        ) {
             this.notifyMissingPassword();
             return false;
-        } else{
+        } else {
             userService.updatePassword(email, oldPWInput, newPWInput, user_id)
                 .then((res) => {
                     console.log(res.data.error);
@@ -235,7 +248,7 @@ class EditProfile extends Component{
                         this.notifyPasswordFailure();
                     } else {
                         this.notifySuccessPw();
-                        window.location.hash="/profile/" + auth.user_id;
+                        window.location.hash = "/profile/" + auth.user_id;
                     }
                 })
                 .catch((err) => {
@@ -246,7 +259,7 @@ class EditProfile extends Component{
     };
 
     save = (e) => {
-        if(this.checkFields()) { // Check if any input-fields are empty
+        if (this.checkFields()) { // Check if any input-fields are empty
             const oldPWInput = document.getElementById("oldPasswordInput").value;
             const newPWInput = document.getElementById("newPasswordInput").value;
 
@@ -255,14 +268,54 @@ class EditProfile extends Component{
             }
             let fileService = new FileService();
             let file = document.getElementById(("fileInput")).files[0];
-            console.log(file.size);
+            /*
             if (file.size > 10000000) { //Bigger than 10 MB
                 this.notifyTooBigFile();
+            } else {
+
+             */
+            if (file !== undefined) {
+                if (file.size > 10000000) {
+                    this.notifyTooBigFile();
+                } else {
+                    fileService.uploadImage(file)
+                        .then((res) => {
+                            this.setState({
+                                img_url: res.data.filePath.filename
+                            })
+                        })
+                        .catch((err) => {
+                            console.log("ERROR:");
+                            console.error(err);
+                        })
+                        .then(() => {
+                            let profileService = new ProfileService();
+                            let newUser = new User(
+                                this.state.user.user_id,
+                                document.getElementById("nameInput").value,
+                                document.getElementById("emailInput").value,
+                                document.getElementById("tlfInput").value,
+                                this.state.img_url,
+                                this.state.user.password,
+                                this.state.user.roleid,
+                                this.state.user.approved
+                            );
+                            console.log(newUser);
+                            profileService.updateUser(newUser)
+                                .then(() => {
+                                    this.notifySuccess();
+                                    window.location.hash = "/profile/" + auth.user_id;
+                                })
+                                .catch((err) => {
+                                    this.notifyFailure();
+                                })
+                        })
+                }
             } else {
                 fileService.uploadImage(file)
                     .then((res) => {
                         this.setState({
-                            img_url : res.data.filePath.filename
+                            img_url: res.data.filePath.filename
                         })
                     })
                     .catch((err) => {
@@ -283,7 +336,7 @@ class EditProfile extends Component{
                         );
                         console.log(newUser);
                         profileService.updateUser(newUser)
-                            .then (() => {
+                            .then(() => {
                                 this.notifySuccess();
                                 window.location.hash = "/profile/" + auth.user_id;
                             })
@@ -294,8 +347,8 @@ class EditProfile extends Component{
             }
 
 
-        }
 
+        }
 
 
         /*
@@ -356,18 +409,18 @@ class EditProfile extends Component{
                 Her ønsker vi å først sjekke om det gamle passordet stemmer med det som ligger i databasen
                 dersom det stemmer skal det nye passordet erstattes med det gamle. dersom passordet ikke stemmer skal check settes til false
                  */
-                //check = false; //if the password doesn't pass the test;
-            /*}
-            if(check){
-                console.log("Alt ok");
-                profileService.updateUser(newUser).catch(e => console.error(e));
-            }
-
+        //check = false; //if the password doesn't pass the test;
+        /*}
+        if(check){
+            console.log("Alt ok");
+            profileService.updateUser(newUser).catch(e => console.error(e));
         }
 
+    }
 
-        console.log(newPassword);
-        console.log(oldPassword);*/
+
+    console.log(newPassword);
+    console.log(oldPassword);*/
 
 
     }

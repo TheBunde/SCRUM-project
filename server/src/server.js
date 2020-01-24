1a45bf97a3641e071ca6eb2d06fdca78d64f3a0a
Simon Årdal <simoaa@stud.ntnu.no>
2020-01-24 13:49:30 +0100

    admin page

--- server/src/server.js
+++ server/src/server.js
@@ -683,7 +683,7 @@ app.put("/profile/:userId/edit", stripToken, (req, res) => {
  * @param {Object} res - Express response object
  */
 
-app.put("/user/:userId/edit", stripToken, (req, res) => {
+app.put("/admin/:userId/edit", stripToken, (req, res) => {
     /**
      * verification using JWT (JSON Web Tokens)
      * [Link](https://jwt.io/)
